9a277c6c8aef3bd991466e88009385d1d68c4b75
Matt Moradi <matthew.moradi@pdx.edu>
2023-05-09 01:48:11 -0700

    Setup Eaten Square Checking

--- src/main.rs
+++ src/main.rs
@@ -37,7 +37,8 @@ fn show_posn(posn: &Chomp)
         let mut rows = 0;
         while rows < posn.nrows
         {
-            print!("#");
+            if(posn.board[rows][columns] == true) {print!("#");}
+            else {print!(".")};
             rows += 1;
         }
         println!();
@@ -60,7 +61,7 @@ fn user_move(posn: &Chomp) -> Option<(usize, usize)>
 {
     let input = input!("Your Move <x> <y>: ");
     let points = input.trim().split(' ').flat_map(str::parse::<usize>).collect::<Vec<_>>();
-    if(points.len() >= 2) {Some((points[0], points[1]))}
+    if(points.len() >= 2 && posn.board[points[1]][points[0]] == true) {Some((points[0], points[1]))}
     else {None}
 }
 
@@ -97,6 +98,8 @@ fn main() {
     let x = parsenum(&args[1]);
     let y = parsenum(&args[2]);
 
-    show_posn(&Chomp {nrows: x, ncols: y, board: [[true; 5]; 4]});
-    user_move(&Chomp {nrows: x, ncols: y, board: [[true; 5]; 4]});
+    let b: [[bool; 3]; 2] = [[false, true, false], [true, false, true]];
+    //show_posn(&Chomp {nrows: x, ncols: y, board: b});
+    //user_move(&Chomp {nrows: x, ncols: y, board: [[true; 5]; 4]});
+    user_move(&Chomp {nrows: x, ncols: y, board: b});
 }
